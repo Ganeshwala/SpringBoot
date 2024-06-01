da1c2ab3961e33f4e91ef676d247fbf073ae64a0
ganeshwala <ganeshwalachintan@gmail.com>
2024-06-01 12:15:16 -0700

    Spring Security with JWT Authentication

---
+++ JWTAuthentication/src/test/java/com/SpringSecurity/JwtAuth/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.JwtAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
